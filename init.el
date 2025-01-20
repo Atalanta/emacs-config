d1013fe756857d5a76b03b2d4c45b913c0f20c80
SNS <stephen.nelsonsmith@ticketmaster.co.uk>
2025-01-20 06:35:28 +0000

    Tidy: remove old lisp

--- init.el
+++
@@ -1,6 +0,0 @@
-;; General UI settings
-(show-paren-mode t)
-                                   
-;; On a mac, allow the '#' key to be pressed
-(global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
-
